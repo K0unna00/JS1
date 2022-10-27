534250f64b8753408711f715a6c154238fb297bd
K0unna00 <78pf4jz@code.edu.az>
2022-10-27 13:07:50 +0400

    First

---
+++ main.js
@@ -0,0 +1,79 @@
+////==========Task 1=========
+// const a = +prompt("Enter number A");
+// const b = +prompt("Enter number B");
+// if (!isNaN(a) && !isNaN(b)) {
+//   console.log(`Result of divide: ${Math.floor(a / b)}`);
+//   console.log(`Result of remainder of the division: ${Math.floor(a % b)}`);
+// } else {
+//   alert("You have to enter only number");
+// }
+
+////==========Task 1=========
+// let a = prompt("Write value's A...");
+// let b = prompt("Write value's B...");
+// let temp;
+// temp = a;
+// a = b;
+// b = temp;
+
+// console.log(`A:${a}`);
+// console.log(`B:${b}`);
+
+////==========Task 2=========
+// A*X+B=C
+// const a = +prompt("Enter number A");
+// const b = +prompt("Enter number B");
+// const c = +prompt("Enter number C");
+// if (!isNaN(a) && !isNaN(b) && a != 0 && b != 0 && c != 0) {
+//   console.log((c - b) / a);
+// }
+
+////==========Task 3=========
+// const a = +prompt("Enter number A");
+// const b = +prompt("Enter number B");
+// if (!isNaN(a) && !isNaN(b)) {
+//   if (a > b) {
+//     console.log(`Result:${a + b}`);
+//   } else if (a === b) {
+//     console.log(`Result:${a * b}`);
+//   } else {
+//     console.log(`Result:${a - b}`);
+//   }
+// } else {
+//   alert("You have to enter only number");
+// }
+
+////==========Task 4=========
+// const a = +prompt(`Enter number A`);
+// const b = +prompt(`Enter number B`);
+// const c = +prompt(`Enter number C`);
+// if (!isNaN(a) && !isNaN(b) && !isNaN(b)) {
+//   const d = b * b - 4 * a * c;
+
+//   console.log(d);
+
+//   if (d > 0) {
+//     const X1 = (-b + Math.sqrt(d)) / (2 * a);
+//     const X2 = (-b - Math.sqrt(d)) / (2 * a);
+
+//     console.log(`X1 = ${X1} ; X2 = ${X2}`);
+//   } else if (d === 0) {
+//     const X = -b / (2 * a);
+
+//     console.log(`X = ${X}`);
+//   } else {
+//     console.log(`ERROR`);
+//   }
+// }
+
+////==========Task 6=========
+// const a = +prompt("Enter number A");
+// const b = +prompt("Enter number B");
+// const c = +prompt("Enter number C");
+// if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+//   if (Math.pow(a, 2) + Math.pow(b, 2) < Math.pow(c, 2)) {
+//     console.log(`(${a}:${b}) this point located inside the circle`);
+//   } else {
+//     console.log(`(${x}:${y}) this point is not located inside the circle`);
+//   }
+// } 
